49f7f71eecbb056073ee939a75cd3892007027c3
Bishwa7 <bishwa244@gmail.com>
2026-03-24 12:27:14 +0530

    Step2 - created a timer using setInterval

--- first-angular-project/src/app/components/profile/profile.ts
+++ first-angular-project/src/app/components/profile/profile.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -8,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class Profile {
   compName = "Profile Component"
+
+  count = signal<number>(0);
+
+
+  callMe() {
+    alert("Button Alert")
+  }
+
+  counter() {
+    const intervalId = setInterval(() => {
+      this.count.update((prev) => prev + 1);
+      console.log(this.count());
+      
+      if(this.count() % 5 === 0 ){
+        clearInterval(intervalId)
+        console.log("Counter Stopped")
+      }
+    }, 1000);
+  }
 }
