8e4bfe1abc1301afa43614ce5816f970f7852f94
Bishwa7 <bishwa244@gmail.com>
2026-04-01 11:34:17 +0530

    P3-S3 - Link and Button (Query Params)

--- routing-project/src/app/components/routing/page/home/home.ts
+++ routing-project/src/app/components/routing/page/home/home.ts
@@ -1,18 +1,30 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
   constructor(public router:Router){}
 
+  users = signal({id:1, name: "Bishwanath Paul", age:25})
+
   goTOProfile() {
     this.router.navigate(["profile"])
   }
-  
+
+  goTOProfileQuery() {
+    this.router.navigate(['profile'], {
+      queryParams:{
+        id: this.users().id,
+        name: this.users().name,
+        age: this.users().age
+      }
+    })
+  }
+
 }
--- routing-project/src/app/components/routing/page/profile/profile.ts
+++ routing-project/src/app/components/routing/page/profile/profile.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,4 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
-export class Profile {}
+export class Profile {
+
+  constructor(public route : ActivatedRoute) {}
+
+  user = signal({
+    id: 0,
+    name: "",
+    age: 0
+  })
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      console.log(params)
+
+      this.user.set({
+        id: Number(params['id']),
+        name: String(params['name']),
+        age: Number(params['age'])
+      })
+    })
+  }
+}
