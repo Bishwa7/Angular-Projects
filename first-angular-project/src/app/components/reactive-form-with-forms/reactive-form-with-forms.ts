c3ff98fe4588b542735053907a26a36c334e7fae
Bishwa7 <bishwa244@gmail.com>
2026-04-06 10:56:23 +0530

    P1-S22 - Validation added with Reactive Form with Forms (formGroup)

--- first-angular-project/src/app/components/reactive-form-with-forms/reactive-form-with-forms.ts
+++ first-angular-project/src/app/components/reactive-form-with-forms/reactive-form-with-forms.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form-with-forms',
@@ -10,12 +10,28 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 export class ReactiveFormWithForms {
 
   loginForm = new FormGroup({
-    name : new FormControl(""),
-    email : new FormControl(""),
-    password : new FormControl("")
+    name : new FormControl("", [Validators.required]),
+    email : new FormControl("", [Validators.required, Validators.email]),
+    password : new FormControl("", [Validators.required, Validators.minLength(5), Validators.maxLength(8)])
   })
 
+  get name() {
+    return this.loginForm.get("name")
+  }
+
+  get email() {
+    return this.loginForm.get("email")
+  }
+
+  get password() {
+    return this.loginForm.get("password")
+  }  
+
   handleSubmit() {
+    if(this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return ;
+    }
     console.log(this.loginForm.value)
   }
 
